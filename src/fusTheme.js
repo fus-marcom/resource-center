8ec9868e2525502a87da2cbbc2d4ff65912dee77
Jesse R Weigel <jesse@jesseweigel.com>
2017-07-21 16:52:31 -0400

    Test custom theme

---
+++ src/fusTheme.js
@@ -0,0 +1,18 @@
+import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
+
+export default {
+    palette: {
+    primary1Color: '#21412a',
+    primary2Color: Colors.cyan700,
+    primary3Color: Colors.lightBlack,
+    accent1Color: Colors.pinkA200,
+    accent2Color: Colors.grey100,
+    accent3Color: Colors.grey500,
+    textColor: Colors.deepPurpleA700,
+    alternateTextColor: Colors.white,
+    canvasColor: Colors.white,
+    borderColor: Colors.grey300,
+    disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
+    pickerHeaderColor: Colors.cyan500,
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#21412a',
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+ReactDOM.render(<MuiThemeProvider muiTheme={muiTheme}><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
