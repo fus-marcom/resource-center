5086a9b9714e69586a2b548ed9421aab43616a63
TehSebaXx <bysebaxx@gmail.com>
2017-07-23 16:21:03 +0200

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import AppBar from 'material-ui/AppBar'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import injectTapEventPlugin from 'react-tap-event-plugin'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import fusTheme from './fusTheme'
+import fusTheme from './fusTheme';
 
-injectTapEventPlugin()
+injectTapEventPlugin();
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {open: false};
-  }
-  render() {
-    return (
-      
-      <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
-        <div>
-          <AppBar
-            title="Title"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-            onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})﻿}
-          />
-          <Drawer
-            open={this.state.open}
-            docked={false}
-            onRequestChange={(open) => this.setState({open})}
-          >
-            <MenuItem>Menu Item</MenuItem>
-            <MenuItem>Menu Item 2</MenuItem>
-          </Drawer>
-        </div>
-      </MuiThemeProvider>
-      
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.state = { open: false };
+	}
+	render() {
+		return (
+			<MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
+				<div>
+					<AppBar
+						title="Title"
+						iconClassNameRight="muidocs-icon-navigation-expand-more"
+						onLeftIconButtonTouchTap={() =>
+							this.setState({ open: !this.state.open })}
+					/>
+					<Drawer
+						open={this.state.open}
+						docked={false}
+						onRequestChange={open => this.setState({ open })}
+					>
+						<MenuItem>Menu Item</MenuItem>
+						<MenuItem>Menu Item 2</MenuItem>
+					</Drawer>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
 
-export default App
+export default App;
