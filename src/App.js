682f17381954edd359e227da702d097888ab9fc8
Jesse Weigel <jesseweigel@gmail.com>
2017-07-24 12:43:51 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Route } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
@@ -11,32 +12,42 @@ import fusTheme from './fusTheme'
 injectTapEventPlugin()
 
 class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = { open: false }
-	}
-	render() {
-		return (
-			<MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
-				<div>
-					<AppBar
-						title="Title"
-						iconClassNameRight="muidocs-icon-navigation-expand-more"
-						onLeftIconButtonTouchTap={() =>
-							this.setState({ open: !this.state.open })}
-					/>
-					<Drawer
-						open={this.state.open}
-						docked={false}
-						onRequestChange={open => this.setState({ open })}
-					>
-						<MenuItem>Menu Item</MenuItem>
-						<MenuItem>Menu Item 2</MenuItem>
-					</Drawer>
-				</div>
-			</MuiThemeProvider>
-		)
-	}
+  constructor (props) {
+    super(props)
+    this.state = { open: false }
+  }
+  render () {
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
+        <div>
+          <AppBar
+            title='Title'
+            iconClassNameRight='muidocs-icon-navigation-expand-more'
+            onLeftIconButtonTouchTap={() =>
+              this.setState({ open: !this.state.open })}
+          />
+          <Drawer
+            open={this.state.open}
+            docked={false}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
+          <Route id='/'>Home View</Route>
+          <Route id='/logos-posters'>Logos and Posters View</Route>
+          <Route id='/logos'>Logos View</Route>
+          <Route id='/posters'>Posters View</Route>
+          <Route id='/letterhead'>Letterhead View</Route>
+          <Route id='/share-a-story'>Share a Story View</Route>
+          <Route id='/planning-guide'>Planning Guide View</Route>
+          <Route id='/services'>Services View</Route>
+          <Route id='/glossary'>Glossary View</Route>
+          <Route id='/service-request-form'>Service Request Form View</Route>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
 }
 
 export default App
