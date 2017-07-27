0896674ee9d9b1bf9c5138c62443755d56cf6a2f
Jesse R Weigel <jesse@jesseweigel.com>
2017-07-27 14:41:25 -0400

    Remove duplicate props

--- src/SideBarItem.js
+++ src/SideBarItem.js
@@ -3,22 +3,21 @@ import { NavLink } from 'react-router-dom'
 import { ListItem } from 'material-ui/List'
 
 class SideBarItem extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       open: false
     }
   }
-  render() {
+  render () {
     return (
       <NavLink
-        activeClassName="active"
+        activeClassName='active'
         exact={this.props.isExact}
         to={this.props.linkTo}
         key={this.props.i}
       >
         <ListItem
-          hoverColor="red"
           primaryText={this.props.primaryText}
           onTouchTap={this.props.handleClose}
           hoverColor='#eee'
