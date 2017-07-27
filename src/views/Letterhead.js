978e8424207260dfba2c7db0f8554b6351eb1080
Jesse R Weigel <jesse@jesseweigel.com>
2017-07-27 15:44:49 -0400

    Add layout for posters and letterhead pages

--- src/views/Letterhead.js
+++ src/views/Letterhead.js
@@ -1,8 +1,84 @@
 import React, { Component } from 'react'
+import { Card, CardTitle, CardActions, CardMedia } from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 
 class Letterhead extends Component {
   render () {
-    return <h1>Letterhead View</h1>
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col s12'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col s12 m6'>
+            <Card>
+              <CardMedia>
+                <img src='https://unsplash.it/600/776/?random' alt='' />
+              </CardMedia>
+              <CardTitle title='Card title' />
+              <CardActions>
+                <FlatButton label='Action1' />
+              </CardActions>
+            </Card>
+          </div>
+          <div className='col s12 m6'>
+            <Card>
+              <CardMedia>
+                <img src='https://unsplash.it/600/776/?random' alt='' />
+              </CardMedia>
+              <CardTitle title='Card title' />
+              <CardActions>
+                <FlatButton label='Action1' />
+              </CardActions>
+            </Card>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col s12 m6'>
+            <Card>
+              <CardMedia>
+                <img src='https://unsplash.it/600/776/?random' alt='' />
+              </CardMedia>
+              <CardTitle title='Card title' />
+              <CardActions>
+                <FlatButton label='Action1' />
+              </CardActions>
+            </Card>
+          </div>
+
+          <div className='col s12 m6'>
+            <Card>
+              <CardMedia>
+                <img src='https://unsplash.it/600/776/?random' alt='' />
+              </CardMedia>
+              <CardTitle title='Card title' />
+              <CardActions>
+                <FlatButton label='Action1' />
+              </CardActions>
+            </Card>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
