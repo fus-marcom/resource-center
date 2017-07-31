f142e79fecacd53837e61491058c9222e4fdff5c
Kevin Peña <kevin.pena.prog@gmail.com>
2017-07-31 04:50:03 -0500

    Add new test. Refactor page visit logic into a function.

--- src/views/Home.test.js
+++ src/views/Home.test.js
@@ -1,11 +1,26 @@
 /* eslint-env jest */
 import nightmare from 'nightmare'
 
+const visit = path => {
+  return nightmare().goto('http://localhost:3000' + path)
+}
+
 describe('Home Page', () => {
   it('contains "Resource Center"', async () => {
-    const page = nightmare().goto('http://localhost:3000/')
+    const page = visit('/')
     const text = await page.evaluate(() => document.body.textContent).end()
 
     expect(text).toContain('Resource Center')
+  })
+
+  it('shows 6 cards', async () => {
+    const page = visit('/')
+    const selector = '.container .col.s12.m4'
+    const cardCount = await page
+      .wait(selector)
+      .evaluate(sel => document.querySelectorAll(sel).length, selector)
+      .end()
+
+    expect(cardCount).toEqual(6)
   })
 })
