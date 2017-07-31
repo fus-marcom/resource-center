7048e326a29839c64c39dc121640bcd5566cdbda
Jesse R Weigel <jesse@jesseweigel.com>
2017-07-31 13:15:34 -0400

    Add request form fields

--- src/views/ServiceRequest.js
+++ src/views/ServiceRequest.js
@@ -1,8 +1,135 @@
 import React, { Component } from 'react'
+import TextField from 'material-ui/TextField'
+import DatePicker from 'material-ui/DatePicker'
+import Checkbox from 'material-ui/Checkbox'
+import RaisedButton from '../MaterializeRaisedButton'
+
+const styles = {
+  block: {
+    maxWidth: 250
+  },
+  checkbox: {
+    marginBottom: 16
+  }
+}
 
 class ServiceRequest extends Component {
   render () {
-    return <h1>Service Request Form View</h1>
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col s12 flow-text'>
+            <h2>Please use this form to request services.</h2>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col s12 m6'>
+            <TextField floatingLabelText='Floating Label Text' fullWidth />
+          </div>
+          <div className='col s12 m6'>
+            <TextField floatingLabelText='Floating Label Text' fullWidth />
+          </div>
+          <div className='col s12 m6'>
+            <TextField floatingLabelText='Floating Label Text' fullWidth />
+          </div>
+          <div className='col s12 m6'>
+            <TextField floatingLabelText='Floating Label Text' fullWidth />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+              floatingLabelText='MultiLine and FloatingLabel'
+              multiLine
+              rows={2}
+              fullWidth
+            />
+          </div>
+          <div className='col s12 m6'>
+            <DatePicker hintText='Portrait Dialog' />
+          </div>
+          <div className='col s12 m6'>
+            <DatePicker hintText='File Upload' />
+          </div>
+          <div className='col s12 m6'>
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+          </div>
+          <div className='col s12 m6'>
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+            <Checkbox label='Simple' style={styles.checkbox} />
+          </div>
+          <div className='col s12'>
+            <RaisedButton label='Submit' primary />
+            <Checkbox label='Simple' style={styles.checkbox} />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
