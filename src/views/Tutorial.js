18cfb15f64ac18ce3a91bccf93b3c08dd558b4aa
Jesse R Weigel <jesse@jesseweigel.com>
2017-08-02 16:27:46 -0400

    Add title to iframe

--- src/views/Tutorial.js
+++ src/views/Tutorial.js
@@ -4,28 +4,29 @@ import '../styles/video.css'
 const Tutorial = props =>
   <div className='container'>
     <div className='row'>
-      <div className='col s12'>
-        <p className='btm-margin-20'>
-          If you have any problems using the MarCom Resource Center, please
-          contact Jesse Weigel at{' '}
-          <a href='mailto:jweigel@franciscan.edu'>jweigel@franciscan.edu</a> or{' '}
-          <a href='tel:17402845305'> 740-284-5305</a>.
-        </p>
-      </div>
-    </div>
-    <div className='row'>
       <div
         className='col s12 valign-wrapper'
         style={{ minHeight: 'calc(100vh - 64px)' }}
       >
-        <div className='video-container z-depth-1'>
-          <iframe
-            src='https://www.youtube.com/embed/WQt0GDsL8ZU?rel=0'
-            width='853'
-            height='480'
-            frameborder='0'
-            allowfullscreen='allowfullscreen'
-          />
+        <div>
+          <div className='video-container z-depth-1'>
+            <iframe
+              src='https://www.youtube.com/embed/WQt0GDsL8ZU?rel=0'
+              width='853'
+              height='480'
+              frameBorder='0'
+              allowFullScreen='allowfullscreen'
+              title='tutorial-video'
+            />
+          </div>
+          <p className='btm-margin-20'>
+            If you have any problems using the MarCom Resource Center, please
+            contact Jesse Weigel at{' '}
+            <a href='mailto:jweigel@franciscan.edu'>
+              jweigel@franciscan.edu
+            </a>{' '}
+            or <a href='tel:17402845305'> 740-284-5305</a>.
+          </p>
         </div>
       </div>
     </div>
