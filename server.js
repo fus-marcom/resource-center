effa9285d1ef3ac444f4547c389809d743bb22d1
J. Patrick Lanigan <planigan@gmail.com>
2017-08-03 21:35:03 -0400

    Edit CORS to be more specific

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const path = require('path')
 const fs = require('fs')
 const formidable = require('formidable')
 const app = express()
-const PORT = process.env.PORT || 9000
+require('dotenv').config()
+const PORT = process.env.SERVER_PORT || 9000
+const CLIENT_PORT = process.env.PORT || 3000
+const PROTOCOL = process.env.PROTOCOL || 'http'
+const HOSTNAME = process.env.HOST || 'localhost'
 const UPLOAD_DIR = path.join(__dirname, 'uploads/')
-const CORS = process.env.NODE_ENV === 'production' ? process.env.HOST : '*'
+const CORS =
+  process.env.NODE_ENV === 'production'
+    ? `${PROTOCOL}://${HOSTNAME}`
+    : `${PROTOCOL}://${HOSTNAME}:${CLIENT_PORT}`
 
 if (!fs.existsSync(UPLOAD_DIR)) {
   console.warn('Creating uploads folder...')
