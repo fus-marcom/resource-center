b349bb13a947f2d5d113cb130c520890904a9454
Jesse R Weigel <jesse@jesseweigel.com>
2017-08-04 16:18:45 -0400

    Test sending emails from server.js via sendgrid

--- server.js
+++ server.js
@@ -2,17 +2,32 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const formidable = require('formidable')
+const helper = require('sendgrid').mail
 const app = express()
 require('dotenv').config()
 const PORT = process.env.SERVER_PORT || 9000
-const CLIENT_PORT = process.env.PORT || 3000
+// const CLIENT_PORT = process.env.PORT || 3000
 const PROTOCOL = process.env.PROTOCOL || 'http'
 const HOSTNAME = process.env.HOST || 'localhost'
 const UPLOAD_DIR = path.join(__dirname, 'uploads/')
 const CORS =
-  process.env.NODE_ENV === 'production'
-    ? `${PROTOCOL}://${HOSTNAME}`
-    : `${PROTOCOL}://${HOSTNAME}:${CLIENT_PORT}`
+  process.env.NODE_ENV === 'production' ? `${PROTOCOL}://${HOSTNAME}` : `*`
+
+const fromEmail = new helper.Email('test@example.com')
+const toEmail = new helper.Email('jesseweigel@gmail.com')
+const subject = 'Sending with SendGrid is Fun'
+const content = new helper.Content(
+  'text/plain',
+  'and easy to do anywhere, even with Node.js'
+)
+const mail = new helper.Mail(fromEmail, subject, toEmail, content)
+
+const sg = require('sendgrid')(process.env.SENDGRID_API_KEY)
+const request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON()
+})
 
 if (!fs.existsSync(UPLOAD_DIR)) {
   console.warn('Creating uploads folder...')
@@ -29,7 +44,9 @@ app.get('/*', function (req, res) {
 app.post('/uploads', function (req, res) {
   var form = new formidable.IncomingForm()
 
-  form.parse(req)
+  form.parse(req).on('field', function (field, value) {
+    console.log(field, value)
+  })
 
   form.on('fileBegin', function (name, file) {
     file.path = `${UPLOAD_DIR}${file.name}`
@@ -45,6 +62,15 @@ app.post('/uploads', function (req, res) {
     'Origin, X-Requested-With, Content-Type, Accept'
   )
   res.status(200).json({ success: true, status: 'Form successfully submitted' })
+
+  sg.API(request, function (error, response) {
+    if (error) {
+      console.log('Error response received')
+    }
+    console.log(response.statusCode)
+    console.log(response.body)
+    console.log(response.headers)
+  })
 })
 
 app.listen(PORT, _ => console.info(`Server listening on PORT ${PORT}...`))
