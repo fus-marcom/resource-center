ee27c73df1c22532f611729f69d7ab1dff2308cc
Jesse R Weigel <jesse@jesseweigel.com>
2017-08-14 12:23:40 -0400

    Add content to posters view

--- src/views/Home.js
+++ src/views/Home.js
@@ -25,7 +25,10 @@ class Home extends Component {
           minHeight: `calc(100vh - ${this.state.topCoord || '64'}px)`
         }}
       >
-        <div className='row' style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div
+          className='row flow-text'
+          style={{ display: 'flex', flexWrap: 'wrap' }}
+        >
           <h2 style={{ flex: '1 100%' }}>Hello! How can MarCom help you?</h2>
           <div className='col s12 m4 flex-div'>
             <GenericCard cardTitle='Brand Manual'>
