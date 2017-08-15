9c9ce96e31d5a0e1047e97c91839002dd9fdf2a9
J. Patrick Lanigan <planigan@gmail.com>
2017-08-15 12:56:44 -0400

    Make entire GenericCard clickable when it's a link

--- src/components/GenericCard.js
+++ src/components/GenericCard.js
@@ -30,62 +30,65 @@ export class GenericCard extends Component {
   }, 32)
 
   render () {
-    const { hoverable, link } = this.props
+    const { link } = this.props
 
     const CardContent = ({
-      headerTitle,
-      headerSubtitle,
-      headerAvatar,
-      mediaImgSrc,
-      mediaImgAlt,
-      overlay,
-      cardTitle,
-      cardSubtitle,
       actions,
+      cardSubtitle,
+      cardTitle,
       children,
-      classes
+      classes,
+      headerAvatar,
+      headerSubtitle,
+      headerTitle,
+      hoverable,
+      mediaImgAlt,
+      mediaImgSrc,
+      overlay,
+      zDepth
     }) =>
-      <Card className={classes} style={{ boxShadow: 'none' }}>
-        {(headerTitle || headerAvatar) &&
-          <CardHeader
-            title={headerTitle}
-            subtitle={headerSubtitle}
-            avatar={headerAvatar}
-          />}
+      <Paper
+        zDepth={zDepth}
+        onMouseOver={hoverable && this.onMouseOver}
+        onMouseOut={hoverable && this.onMouseOut}
+        style={{ height: '100%' }}
+      >
+        <Card className={classes} style={{ boxShadow: 'none' }}>
+          {(headerTitle || headerAvatar) &&
+            <CardHeader
+              title={headerTitle}
+              subtitle={headerSubtitle}
+              avatar={headerAvatar}
+            />}
 
-        {mediaImgSrc &&
-          <CardMedia className='img-container' overlay={overlay}>
-            <img src={mediaImgSrc} alt={mediaImgAlt} />
-          </CardMedia>}
-        {cardTitle && <CardTitle title={cardTitle} subtitle={cardSubtitle} />}
-        <CardText style={{ fontSize: '16px' }}>
-          {children}
-        </CardText>
-        {actions &&
-          <CardActions className='card-actions'>
-            {actions}
-          </CardActions>}
-      </Card>
+          {mediaImgSrc &&
+            <CardMedia className='img-container' overlay={overlay}>
+              <img src={mediaImgSrc} alt={mediaImgAlt} />
+            </CardMedia>}
+          {cardTitle && <CardTitle title={cardTitle} subtitle={cardSubtitle} />}
+          <CardText style={{ fontSize: '16px' }}>
+            {children}
+          </CardText>
+          {actions &&
+            <CardActions className='card-actions'>
+              {actions}
+            </CardActions>}
+        </Card>
+      </Paper>
 
     const isInternal = link && link[0] === '/'
 
-    return (
-      <Paper
-        zDepth={this.state.depth}
-        onMouseOver={hoverable && this.onMouseOver}
-        onMouseOut={hoverable && this.onMouseOut}
-      >
-        {link
-          ? isInternal
-            ? <Link to={link}>
-              <CardContent {...this.props} />
-            </Link>
-            : <a href={link} target='_blank'>
-              <CardContent {...this.props} />
-            </a>
-          : <CardContent {...this.props} />}
-      </Paper>
-    )
+    const jsx = link
+      ? isInternal
+        ? <Link to={link}>
+          <CardContent {...this.props} zDepth={this.state.depth} />
+        </Link>
+        : <a href={link} target='_blank'>
+          <CardContent {...this.props} zDepth={this.state.depth} />
+        </a>
+      : <CardContent {...this.props} zDepth={this.state.depth} />
+
+    return jsx
   }
 }
 
