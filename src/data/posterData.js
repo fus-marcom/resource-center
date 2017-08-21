375de1d1692e79597f5dbc3be04a09c62a184c69
Matt Carlotta <carlotta.matt@gmail.com>
2017-08-20 18:11:54 -0700

    Refactor Posters.js view to reduce redundancy

---
+++ src/data/posterData.js
@@ -0,0 +1,58 @@
+export const posterData = [
+  {
+    cardTitle: 'Create Your Own',
+    link: 'https://www.canva.com/',
+    hoverable: true,
+    description:
+      'Canva.com is a free graphic design tool with an easy to use drag-and-drop interface and access to over a million photographs, graphics, and fonts. It is used by non-designers as well as professionals.'
+  },
+  {
+    cardTitle: 'Video Tutorials',
+    link: '/poster-videos',
+    hoverable: true,
+    description:
+      'Learn how to create posters in Powerpoint, Publisher, Photoshop, and InDesign.'
+  },
+  {
+    cardTitle: 'Student Design',
+    description:
+      'Starting fall 2017 semester, students with graphic design experience will be available to create your flyer or poster. Contact Vicki Bell, Print Services Manager at Ext. 6354, for pricing and more information.'
+  },
+  {
+    cardTitle: 'Professional Design',
+    description:
+      'Marie Highfield ’91 is a freelance graphic designer who lives in Steubenville and does many design projects for the University.',
+    contactInfo: [
+      {
+        link: 'tel:17402664926',
+        linkText: '740-266-4926'
+      },
+      {
+        link: 'mailto:mariehighfield@sbcglobal.net',
+        linkText: 'mariehighfield@sbcglobal.net'
+      }
+    ]
+  },
+  {
+    cardTitle: 'Printing',
+    description:
+      'Franciscan University Mail and Print Services prices are well below those of area printers. They print in a variety of formats including table tents, flyers, and posters in sizes from 8-1/2x11” to 24x36” or bigger. Contact Mail and Print Services for a pricing sheet and format guidelines. The manager is Vicki Bell.',
+    contactInfo: [
+      {
+        link: 'tel:17402836354',
+        linkText: '740-283-6354'
+      },
+      {
+        link: 'mailto:mariehighfield@sbcglobal.net',
+        linkText: 'mariehighfield@sbcglobal.net'
+      }
+    ]
+  },
+  {
+    cardTitle: 'Logos',
+    link: '/logos',
+    hoverable: true,
+    description:
+      'For all projects, please include the official Franciscan University logo, either in full color or one color of your choice.'
+  }
+]
