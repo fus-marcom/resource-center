465d59c0a1742764540ebedbad2245a0ed351add
Jesse R Weigel <jesse@jesseweigel.com>
2017-08-31 17:09:57 -0400

    Add icon to checkbox label

--- src/views/ServiceRequest.js
+++ src/views/ServiceRequest.js
@@ -52,7 +52,19 @@ class ServiceRequest extends Component {
       'Comments'
     ]
     this.leftCheckboxes = [
-      'Simple1',
+      <span>
+        Photography
+        <svg
+          fill='#000000'
+          height='24'
+          viewBox='0 0 24 24'
+          width='24'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path d='M0 0h24v24H0z' fill='none' />
+          <path d='M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' />
+        </svg>
+      </span>,
       'Simple2',
       'Simple3',
       'Simple4',
@@ -77,16 +89,16 @@ class ServiceRequest extends Component {
       }),
       {}
     )
-    const checkboxProps = [
-      ...this.leftCheckboxes,
-      ...this.rightCheckboxes
-    ].reduce(
-      (acc, label) => ({
-        [this.formatLabelToProperty(label)]: false,
-        ...acc
-      }),
-      {}
-    )
+    // const checkboxProps = [
+    //   ...this.leftCheckboxes,
+    //   ...this.rightCheckboxes
+    // ].reduce(
+    //   (acc, label) => ({
+    //     [this.formatLabelToProperty(label)]: false,
+    //     ...acc
+    //   }),
+    //   {}
+    // )
     this.state = {
       form: {
         fileInput: null
@@ -96,7 +108,7 @@ class ServiceRequest extends Component {
       resultDialogText: null,
       resultDialogSuccess: true
     }
-    Object.assign(this.state.form, stringProps, checkboxProps)
+    Object.assign(this.state.form, stringProps)
     this.handleInputChange = this.handleInputChange.bind(this)
   }
 
@@ -205,8 +217,8 @@ class ServiceRequest extends Component {
     const CheckboxField = (label, index) => (
       <Checkbox
         label={label}
-        name={this.formatLabelToProperty(label)}
-        checked={this.state.form[this.formatLabelToProperty(label)]}
+        name='test'
+        checked={this.state.form['test']}
         key={index}
         onCheck={this.handleInputChange}
         style={styles.checkbox}
