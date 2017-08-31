19eee0efdc51810f688ef9197722df9ba782d021
Sebastian <bysebaxx@gmail.com>
2017-08-31 16:58:28 +0200

    Filter files extensions in selection screen

--- src/views/ServiceRequest.js
+++ src/views/ServiceRequest.js
@@ -16,6 +16,9 @@ import Formsy from 'formsy-react'
 import { FormsyText, FormsyCheckbox } from 'formsy-material-ui/lib'
 import { singleLineFields, multiLineFields } from '../data/serviceRequestFields'
 
+const fileExtensions =
+  'application/vnd.rar, application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/vnd.openxmlformats-officedocument.presentationml.presentation, audio/mp4, audio/mpeg, text/plain, application/zip, video/quicktime, video/avi, audio/wav, image/jpeg, application/octet-stream, image/png'
+
 const PORT = process.env.UPLOADS_PORT || 9000
 const HOST = process.env.UPLOADS_HOST || window.location.host.split(':')[0]
 const UPLOAD_URL = `http://${HOST}:${PORT}/uploads`
@@ -223,6 +226,7 @@ class ServiceRequest extends Component {
                     name='upload[]'
                     type='file'
                     multiple
+                    accept={fileExtensions}
                     onChange={this.handleFilePath}
                     ref={input => {
                       this.uploadInput = input
