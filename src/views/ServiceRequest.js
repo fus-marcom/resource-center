66820e46e140d9e04e4423315485572692478730
Jesse R Weigel <jesse@jesseweigel.com>
2017-09-07 11:48:59 -0400

    Fix path for production uploads

--- src/views/ServiceRequest.js
+++ src/views/ServiceRequest.js
@@ -25,9 +25,14 @@ import {
 const fileExtensions =
   'application/vnd.rar, application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/vnd.openxmlformats-officedocument.presentationml.presentation, audio/mp4, audio/mpeg, text/plain, application/zip, video/quicktime, video/avi, audio/wav, image/jpeg, application/octet-stream, image/png'
 
+const PROTOCOL = process.env.PROTOCOL || 'http'
+const HOSTNAME = process.env.HOST || 'localhost'
 const PORT = process.env.UPLOADS_PORT || 9000
 const HOST = process.env.UPLOADS_HOST || window.location.host.split(':')[0]
-const UPLOAD_URL = `http://${HOST}:${PORT}/uploads`
+const UPLOAD_URL =
+  process.env.NODE_ENV === 'production'
+    ? `${PROTOCOL}://${HOSTNAME}`
+    : `http://${HOST}:${PORT}/uploads`
 
 const styles = {
   checkbox: {
