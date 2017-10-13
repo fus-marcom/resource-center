cb18247c0795836558ceb3f82abcb113cc306658
SebastianCApostolescu <sebastianc.apostolescu@gmail.com>
2017-10-13 13:05:29 +0200

    Security fix

--- src/components/GenericCard.js
+++ src/components/GenericCard.js
@@ -23,48 +23,53 @@ export class GenericCard extends Component {
       mediaImgAlt,
       mediaImgSrc,
       overlay
-    }) =>
+    }) => (
       <Card
         className={`${className} card z-depth-1`}
         style={{ height: '100%' }}
       >
-        {(headerTitle || headerAvatar) &&
+        {(headerTitle || headerAvatar) && (
           <CardHeader
             title={headerTitle}
             subtitle={headerSubtitle}
             avatar={headerAvatar}
-          />}
+          />
+        )}
 
-        {mediaImgSrc &&
+        {mediaImgSrc && (
           <CardMedia className='img-container' overlay={overlay}>
             <img src={mediaImgSrc} alt={mediaImgAlt} />
-          </CardMedia>}
-        {cardTitle &&
+          </CardMedia>
+        )}
+        {cardTitle && (
           <CardTitle
             title={cardTitle}
             subtitle={cardSubtitle}
             style={{ paddingBottom: '0' }}
-          />}
+          />
+        )}
         <CardText style={{ fontSize: '16px', paddingTop: '0' }}>
           {children}
         </CardText>
-        {actions &&
-          <CardActions className='card-actions'>
-            {actions}
-          </CardActions>}
+        {actions && (
+          <CardActions className='card-actions'>{actions}</CardActions>
+        )}
       </Card>
+    )
 
     const isInternal = link && link[0] === '/'
 
-    const jsx = link
-      ? isInternal
-        ? <Link to={link}>
-          <CardContent {...this.props} />
-        </Link>
-        : <a href={link} target='_blank'>
-          <CardContent {...this.props} />
-        </a>
-      : <CardContent {...this.props} />
+    const jsx = link ? isInternal ? (
+      <Link to={link}>
+        <CardContent {...this.props} />
+      </Link>
+    ) : (
+      <a href={link} rel='noopener noreferrer' target='_blank'>
+        <CardContent {...this.props} />
+      </a>
+    ) : (
+      <CardContent {...this.props} />
+    )
 
     return jsx
   }
